deacb9dffc600ce0866aa75dea456037b60d0c39
Synthird <105394020+Synthird@users.noreply.github.com>
2024-11-12 22:17:17 -0800

    Moved dialog variables to the top

--- src/CharacterCounter.java
+++ src/CharacterCounter.java
@@ -1,15 +1,14 @@
 import javax.swing.JOptionPane;
 
 public class CharacterCounter {
-    public CharacterCounter(int characters) {
-        String paneTitle = "Number of characters";
-        int paneType = JOptionPane.PLAIN_MESSAGE;
+    private static String paneTitle = "Number of Characters";
+    private static int paneType = JOptionPane.PLAIN_MESSAGE;
 
+    public CharacterCounter(int characters) {
         if (characters == 1) {
             JOptionPane.showMessageDialog(null, "1 character", paneTitle, paneType);
         } else {
             JOptionPane.showMessageDialog(null, String.format("%s characters", characters), paneTitle, paneType);
         }
     }
-
 }
